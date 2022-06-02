09c091d8aff66fb95f01525fb95a2281bc0ed6b7
user <user@210.204.203.201>
2022-06-02 21:32:01 +0900

    주석달고 평균점수 소수점 첫째자리까지 반영하여 결과 출력

--- Git-Practice/src/Git_Practice.java
+++ Git-Practice/src/Git_Practice.java
@@ -2,31 +2,31 @@ import java.util.Scanner;
 public class Git_Practice {
 	public static void main(String[] args) {
 		System.out.print("국어, 영어, 수학, 사회, 과학 점수를 차례로 입력하시오: ");		
-		Scanner scanner = new Scanner(System.in);
+		Scanner s = new Scanner(System.in);
 		
-		int 국어 = scanner.nextInt();
+		int 국어 = s.nextInt();			// 국어과목의 점수를 입력받음
 		System.out.print("국어:"+국어+", ");
 		
-		int 영어 = scanner.nextInt();
+		int 영어 = s.nextInt();			// 영어과목의 점수를 입력받음
 		System.out.print("영어:"+영어+", ");
 		
-		int 수학 = scanner.nextInt();
+		int 수학 = s.nextInt();			// 수학과목의 점수를 입력받음
 		System.out.print("수학:"+수학+", ");
 		
-		int 사회 = scanner.nextInt();
+		int 사회 = s.nextInt();			// 사회과목의 점수를 입력받음
 		System.out.print("사회:"+사회+", ");
 		
-		int 과학 = scanner.nextInt();
+		int 과학 = s.nextInt();			// 과학과목의 점수를 입력받음
 		System.out.print("과학:"+과학+", ");
 		
-		int 평균 = ((국어+영어+수학+사회+과학)/5)+((국어+영어+수학+사회+과학)%5);
+		double 평균 = ((국어+영어+수학+사회+과학)/5.0); // 입력받은 5과목의 총 평균을 소수점 첫째자리까지 구하는 변수
 		System.out.println("평균은:"+평균);
-		if(평균 >= 80) {
-			System.out.println("합격입니다.");
+		if(평균 >= 80) {                   // 구한 평균이 합격인지 불합격인지 판별
+			System.out.println("합격입니다.");    
 		}
 		else {
 			System.out.println("불합격입니다.");
 		}
-		scanner.close();
+		s.close();
 	}
 }
